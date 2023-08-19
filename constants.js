401f667e5accb4e287a4aa2dd5571819af00f01f
Himanshu <prjapathimanshu51@gmail.com>
2023-08-19 21:26:06 +0530

    Addd nft project in portfolio

--- constants.js
+++ constants.js
@@ -16,37 +16,44 @@ export const portfolio = [{
     desc: "Inventory management system is for razererp to manage their inventory",
     subtitle: "Windows/Mac  - Flutter",
     image: "/images/portfolio/inventory.png",
-}, {
-    title: "Pioneer Cares",
-    date: "2022 - Previous",
-    desc: "App for cable technicians to reward on purchase of the table with coupons",
-    subtitle: "Mobile | Tablet - Flutter Mobile App",
-    image: "/images/portfolio/pioneer.png",
-}, {
-    title: "Lmprofit Desktop Pos System ",
-    date: "2022 - Previous",
-    desc: "Pos system with invoice and silent printing for Windows/Mac",
-    subtitle: "Windows/Mac - Electron,Vue Js ",
-    image: "/images/portfolio/lmprofit.png",
-}, {
-    title: "Lmprofit Web Pos System ",
-    date: "2022 - Previous",
-    desc: "Pos system with invoice and silent printing for web",
-    subtitle: "Web - Laravel ",
-    image: "/images/portfolio/lmprofit_web.png",
-}, {
-    title: "Cricket Net",
-    date: "2022 - Previous",
-    desc: "Cricket Prediction Mobile app with affiliate and news section",
-    subtitle: "Android - Java,Kotlin ",
-    image: "/images/portfolio/cricnet.png",
-}, {
-    title: "Movies App | Youtube similar",
-    date: "2020 - 2021 ",
-    desc: "Movies app for playing youtube videos fetch from custom laravel backend",
-    subtitle: "Android - Java,Kotlin ",
-    image: "/images/portfolio/movies_app.png",
 },
+    {
+        title: "NFT Landing Page ",
+        date: "2023 - Current",
+        desc: "Nft landing page for a new nft startup",
+        subtitle: "Web/Mobile  - Next Js | Tailwind css",
+        image: "/images/portfolio/nft.png",
+    }, {
+        title: "Pioneer Cares",
+        date: "2022 - Previous",
+        desc: "App for cable technicians to reward on purchase of the table with coupons",
+        subtitle: "Mobile | Tablet - Flutter Mobile App",
+        image: "/images/portfolio/pioneer.png",
+    }, {
+        title: "Lmprofit Desktop Pos System ",
+        date: "2022 - Previous",
+        desc: "Pos system with invoice and silent printing for Windows/Mac",
+        subtitle: "Windows/Mac - Electron,Vue Js ",
+        image: "/images/portfolio/lmprofit.png",
+    }, {
+        title: "Lmprofit Web Pos System ",
+        date: "2022 - Previous",
+        desc: "Pos system with invoice and silent printing for web",
+        subtitle: "Web - Laravel ",
+        image: "/images/portfolio/lmprofit_web.png",
+    }, {
+        title: "Cricket Net",
+        date: "2022 - Previous",
+        desc: "Cricket Prediction Mobile app with affiliate and news section",
+        subtitle: "Android - Java,Kotlin ",
+        image: "/images/portfolio/cricnet.png",
+    }, {
+        title: "Movies App | Youtube similar",
+        date: "2020 - 2021 ",
+        desc: "Movies app for playing youtube videos fetch from custom laravel backend",
+        subtitle: "Android - Java,Kotlin ",
+        image: "/images/portfolio/movies_app.png",
+    },
 
 ]
 
