32c7317110d509ed874fd83dd7f80c284faf61ff
Himanshu <prjapathimanshu51@gmail.com>
2024-11-02 13:31:43 +0530

    imge view

--- src/components/front/home/PortfolioItem.js
+++ src/components/front/home/PortfolioItem.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Image from "next/image";
+import {useModalProvider} from "../../../context/ModalProvider";
+import Modal from "../../common/Modal";
 
 const PortfolioItem = ({date,title,subtitle,desc,image,index}) => {
+
     return (
         <div className={`portfolio portfolio-${index}  w-full card p-3 lg:p-8 flex flex-col lg:flex-row  items-center translate-y-24`} key={title}>
 
             <div className={'w-full h-[250px] overflow-hidden rounded-lg  lg:w-[300px] lg:h-[220px] relative '}>
-                <Image src={image} className={'rounded-lg overflow-hidden object-contain bg-portfolio_image_bg w-full h-full hover:scale-110  transition-all duration-300 cursor-pointer'} height={1280} width={759}
-                       alt={"portfolio"}/>
+                <a href={image}>
+                    <Image  src={image} className={'rounded-lg overflow-hidden object-contain bg-portfolio_image_bg w-full h-full hover:scale-110  transition-all duration-300 cursor-pointer'} height={1280} width={759}
+                            alt={"portfolio"}/>
+                </a>
             </div>
             <div className={'flex-1 p-4 flex flex-col gap-5 items-start'}>
 
